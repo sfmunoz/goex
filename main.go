f200a4801e01196179bc515a68f1ae99d6796e60
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2023-10-06 17:27:26 +0000

    main.go: EXAMPLES array holds functions to be called

--- main.go
+++ main.go
@@ -28,9 +28,9 @@ import (
 // }}}
 // {{{ globals
 
-var EXAMPLES = [][]string{
-	{"pointers_refs", "pointers and references"},
-	{"structs_ints", "structures and interfaces"},
+var EXAMPLES = [][]any{
+	{"pointers_refs", "pointers and references", func() { pr.Main() }},
+	{"structs_ints", "structures and interfaces", si.Main},
 }
 
 // }}}
@@ -46,17 +46,17 @@ func usage() {
 	fmt.Println("")
 	fmt.Println("Examples:")
 	fmt.Println("")
-	fmt.Println("  $ go run main.go " + EXAMPLES[0][0])
+	fmt.Println("  $ go run main.go " + EXAMPLES[0][0].(string))
 	fmt.Println("  $ go run main.go 1")
 	fmt.Println("")
 	fmt.Println("Available examples:")
 	fmt.Println("")
 	top := -1
 	for _, v := range EXAMPLES {
-		top = max(top, len(v[0]))
+		top = max(top, len(v[0].(string)))
 	}
 	for i, v := range EXAMPLES {
-		fmt.Printf("%3d: %s %s %s\n", i+1, v[0], strings.Repeat(".", top-len(v[0])+3), v[1])
+		fmt.Printf("%3d: %s %s %s\n", i+1, v[0], strings.Repeat(".", top-len(v[0].(string))+3), v[1])
 	}
 	fmt.Println("")
 	fmt.Println("Reference:")
@@ -83,15 +83,13 @@ func main() {
 	ex := flag.Arg(0)
 	exN, err := strconv.Atoi(ex)
 	if err == nil && exN >= 1 && exN <= len(EXAMPLES) {
-		ex = EXAMPLES[exN-1][0]
+		ex = EXAMPLES[exN-1][0].(string)
 	}
-	switch ex {
-	case "pointers_refs":
-		pr.Main()
-		os.Exit(0)
-	case "structs_ints":
-		si.Main()
-		os.Exit(0)
+	for _, v := range EXAMPLES {
+		if v[0] == ex {
+			v[2].(func())()
+			os.Exit(0)
+		}
 	}
 	fmt.Println("error: unknown example '" + ex + "'")
 	os.Exit(1)
